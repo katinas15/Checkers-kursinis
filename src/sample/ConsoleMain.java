b4e8f4c71fc5e077b5ea9affc562587e2342f6ec
katinas <laimonas.janutenas@gmail.com>
2019-10-27 18:11:11 +0200

    idetas valdymas

--- src/sample/ConsoleMain.java
+++ src/sample/ConsoleMain.java
@@ -1,16 +1,36 @@
 package sample;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 import static sample.GameBoard.tableHeight;
 import static sample.GameBoard.tableWidth;
 
 public class ConsoleMain {
-    static GameBoard gameBoard = new GameBoard();
-    public static void main(String[] args){
-        printGame();
+
+    public static void main(String[] args) throws IOException {
+        GameBoard gameBoard = new GameBoard();
+        Scanner keyboard = new Scanner(System.in);
+        while(true){
+            printGame(gameBoard);
+            int pieceX = Integer.parseInt(keyboard.nextLine().trim());
+            int pieceY = Integer.parseInt(keyboard.nextLine().trim());
+            int toX = Integer.parseInt(keyboard.nextLine().trim());
+            int toY = Integer.parseInt(keyboard.nextLine().trim());
+            gameBoard.movePiece(pieceX,pieceY,toX, toY);
+        }
     }
 
-    private static void printGame(){
+    private static void printGame(GameBoard gameBoard){
+        System.out.print(" ");
+        for(int j = 0;j<tableWidth;j++){
+            System.out.print(j);
+
+        }
+        System.out.println();
+
         for(int i = 0;i<tableHeight;i++){
+            System.out.print(i);
             for(int j = 0;j<tableWidth;j++){
                 if(gameBoard.getTile(i,j) == 2) System.out.print("X");
                 else if(gameBoard.getTile(i,j) == 1) System.out.print("Y");
@@ -19,4 +39,6 @@ public class ConsoleMain {
             System.out.println();
         }
     }
+
+
 }
--- src/sample/GameBoard.java
+++ src/sample/GameBoard.java
@@ -19,4 +19,37 @@ public class GameBoard {
     public int getTile(int x,int y){
         return board[x][y];
     }
+
+    public void movePiece(int pieceX, int pieceY, int toX, int toY){
+        int piece = board[pieceY][pieceX];
+        if(!checkPiece(piece)) return;
+
+        if(checkSingleStep(piece%2==0, pieceX, pieceY, toX, toY)){
+            board[pieceY][pieceX] = board[pieceY][pieceX];
+            board[toY][toX] = piece;
+        }
+    }
+
+    private boolean checkPiece(int piece){
+        if(piece == 0) {
+            System.out.println("incorrect piece");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkSingleStep(boolean color, int pieceX, int pieceY, int toX, int toY){
+        if(color){
+            if( (pieceX + 1) == toX && (pieceY + 1) == toY) return true;
+            if( (pieceX - 1) == toX && (pieceY + 1) == toY) return true;
+        } else {
+            if( (pieceX + 1) == toX && (pieceY - 1) == toY) return true;
+            if( (pieceX - 1) == toX && (pieceY - 1) == toY) return true;
+        }
+
+        System.out.println("incorrect destination");
+        return false;
+
+    }
+
 }
